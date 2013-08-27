666c7b546495b0e945f4402e8010f63d23d73d0e
lkirill <kirillz@lodoss.org>
2013-08-27 15:35:54 +0400

    starting to implement "crop" function

--- src/imagemagick.cc
+++ src/imagemagick.cc
@@ -8,7 +8,7 @@
 #include <exception>
 
 #define THROW_ERROR_EXCEPTION(x) ThrowException(v8::Exception::Error(String::New(x))); \
-    scope.Close(Undefined())
+  scope.Close(Undefined())
 
 // input
 //   args[ 0 ]: options. required, object with following key,values
@@ -22,148 +22,302 @@
 //                  debug:       optional. 1 or 0
 //              }
 Handle<Value> Convert(const Arguments& args) {
-    HandleScope scope;
-    MagickCore::SetMagickResourceLimit(MagickCore::ThreadResource, 1);
+  HandleScope scope;
+  MagickCore::SetMagickResourceLimit(MagickCore::ThreadResource, 1);
 
-    if ( args.Length() != 1 ) {
-        return THROW_ERROR_EXCEPTION("convert() requires 1 (option) argument!");
+  if ( args.Length() != 1 ) {
+    return THROW_ERROR_EXCEPTION("convert() requires 1 (option) argument!");
+  }
+  if ( ! args[ 0 ]->IsObject() ) {
+    return THROW_ERROR_EXCEPTION("convert()'s 1st argument should be an object");
+  }
+  Local<Object> obj = Local<Object>::Cast( args[ 0 ] );
+
+  Local<Object> srcData = Local<Object>::Cast( obj->Get( String::NewSymbol("srcData") ) );
+  if ( srcData->IsUndefined() || ! node::Buffer::HasInstance(srcData) ) {
+    return THROW_ERROR_EXCEPTION("convert()'s 1st argument should have \"srcData\" key with a Buffer instance");
+  }
+
+  int debug = obj->Get( String::NewSymbol("debug") )->Uint32Value();
+  if (debug) printf( "debug: on\n" );
+
+  Magick::Blob srcBlob( node::Buffer::Data(srcData), node::Buffer::Length(srcData) );
+
+  Magick::Image image;
+  try {
+    image.read( srcBlob );
+  }
+  catch (std::exception& err) {
+    std::string message = "image.read failed with error: ";
+    message            += err.what();
+    return THROW_ERROR_EXCEPTION(message.c_str());
+  }
+  catch (...) {
+    return THROW_ERROR_EXCEPTION("unhandled error");
+  }
+
+  if (debug) printf("original width,height: %d, %d\n", (int) image.columns(), (int) image.rows());
+
+  unsigned int width = obj->Get( String::NewSymbol("width") )->Uint32Value();
+  if (debug) printf( "width: %d\n", width );
+
+  unsigned int height = obj->Get( String::NewSymbol("height") )->Uint32Value();
+  if (debug) printf( "height: %d\n", height );
+
+  Local<Value> resizeStyleValue = obj->Get( String::NewSymbol("resizeStyle") );
+  const char* resizeStyle = "aspectfill";
+  String::AsciiValue resizeStyleAsciiValue( resizeStyleValue->ToString() );
+  if ( ! resizeStyleValue->IsUndefined() ) {
+    resizeStyle = *resizeStyleAsciiValue;
+  }
+  if (debug) printf( "resizeStyle: %s\n", resizeStyle );
+
+  Local<Value> formatValue = obj->Get( String::NewSymbol("format") );
+  String::AsciiValue format( formatValue->ToString() );
+  if ( ! formatValue->IsUndefined() ) {
+    if (debug) printf( "format: %s\n", *format );
+    image.magick( *format );
+  }
+
+  if ( width || height ) {
+    if ( ! width  ) { width  = image.columns(); }
+    if ( ! height ) { height = image.rows();    }
+
+    // do resize
+    if ( strcmp( resizeStyle, "aspectfill" ) == 0 ) {
+      // ^ : Fill Area Flag ('^' flag)
+      // is not implemented in Magick++
+      // and gravity: center, extent doesnt look like working as exptected
+      // so we do it ourselves
+
+      // keep aspect ratio, get the exact provided size, crop top/bottom or left/right if necessary
+      double aspectratioExpected = (double)height / (double)width;
+      double aspectratioOriginal = (double)image.rows() / (double)image.columns();
+      unsigned int xoffset = 0;
+      unsigned int yoffset = 0;
+      unsigned int resizewidth;
+      unsigned int resizeheight;
+      if ( aspectratioExpected > aspectratioOriginal ) {
+        // expected is taller
+        resizewidth  = (unsigned int)( (double)height / (double)image.rows() * (double)image.columns() + 1. );
+        resizeheight = height;
+        xoffset      = (unsigned int)( (resizewidth - width) / 2. );
+        yoffset      = 0;
+      }
+      else {
+        // expected is wider
+        resizewidth  = width;
+        resizeheight = (unsigned int)( (double)width / (double)image.columns() * (double)image.rows() + 1. );
+        xoffset      = 0;
+        yoffset      = (unsigned int)( (resizeheight - height) / 2. );
+      }
+
+      if (debug) printf( "resize to: %d, %d\n", resizewidth, resizeheight );
+      Magick::Geometry resizeGeometry( resizewidth, resizeheight, 0, 0, 0, 0 );
+      image.resize( resizeGeometry );
+
+      // limit canvas size to cropGeometry
+      if (debug) printf( "crop to: %d, %d, %d, %d\n", width, height, xoffset, yoffset );
+      Magick::Geometry cropGeometry( width, height, xoffset, yoffset, 0, 0 );
+
+      Magick::Color transparent( "white" );
+      if ( strcmp( *format, "PNG" ) == 0 ) {
+        // make background transparent for PNG
+        // JPEG background becomes black if set transparent here
+        transparent.alpha( 1. );
+      }
+      image.extent( cropGeometry, transparent );
     }
-    if ( ! args[ 0 ]->IsObject() ) {
-        return THROW_ERROR_EXCEPTION("convert()'s 1st argument should be an object");
+    else if ( strcmp ( resizeStyle, "aspectfit" ) == 0 ) {
+      // keep aspect ratio, get the maximum image which fits inside specified size
+      char geometryString[ 32 ];
+      sprintf( geometryString, "%dx%d", width, height );
+      if (debug) printf( "resize to: %s\n", geometryString );
+      image.resize( geometryString );
     }
-    Local<Object> obj = Local<Object>::Cast( args[ 0 ] );
+    else if ( strcmp ( resizeStyle, "fill" ) == 0 ) {
+      // change aspect ratio and fill specified size
+      char geometryString[ 32 ];
+      sprintf( geometryString, "%dx%d!", width, height );
+      if (debug) printf( "resize to: %s\n", geometryString );
+      image.resize( geometryString );
+    }
+    else {
+      return THROW_ERROR_EXCEPTION("resizeStyle not supported");
+    }
+    if (debug) printf( "resized to: %d, %d\n", (int)image.columns(), (int)image.rows() );
+  }
 
-    Local<Object> srcData = Local<Object>::Cast( obj->Get( String::NewSymbol("srcData") ) );
-    if ( srcData->IsUndefined() || ! node::Buffer::HasInstance(srcData) ) {
-        return THROW_ERROR_EXCEPTION("convert()'s 1st argument should have \"srcData\" key with a Buffer instance");
+  unsigned int quality = obj->Get( String::NewSymbol("quality") )->Uint32Value();
+  if ( quality ) {
+    if (debug) printf( "quality: %d\n", quality );
+    image.quality( quality );
+  }
+
+  Magick::Blob dstBlob;
+  image.write( &dstBlob );
+
+  node::Buffer* retBuffer = node::Buffer::New( dstBlob.length() );
+  memcpy( node::Buffer::Data( retBuffer->handle_ ), dstBlob.data(), dstBlob.length() );
+  return scope.Close( retBuffer->handle_ );
+}
+
+// input
+//   args[ 0 ]: options. required, object with following key,values
+//              {
+//                  srcData:     required. Buffer with binary image data
+//                  left:        required. % defines left corner crop position
+//                  top:         required. % defines top corner crop position
+//                  quality:     optional. 0-100 integer, default 75. JPEG/MIFF/PNG compression level.
+//                  width:       optional. % default image width - left
+//                  height:      optional. % default image height - top
+//                  format:      optional. one of http://www.imagemagick.org/script/formats.php ex: "JPEG"
+//                  debug:       optional. 1 or 0
+//              }
+// TODO: convert into crop function
+Handle<Value> Crop(const Arguments& args) {
+  HandleScope scope;
+  MagickCore::SetMagickResourceLimit(MagickCore::ThreadResource, 1);
+
+  if ( args.Length() != 1 ) {
+    return THROW_ERROR_EXCEPTION("crop() requires 1 (option) argument!");
+  }
+  if ( ! args[ 0 ]->IsObject() ) {
+    return THROW_ERROR_EXCEPTION("crop()'s 1st argument should be an object");
+  }
+  Local<Object> obj = Local<Object>::Cast( args[ 0 ] );
+
+  Local<Object> srcData = Local<Object>::Cast( obj->Get( String::NewSymbol("srcData") ) );
+  if ( srcData->IsUndefined() || ! node::Buffer::HasInstance(srcData) ) {
+    return THROW_ERROR_EXCEPTION("crop()'s 1st argument should have \"srcData\" key with a Buffer instance");
+  }
+
+  Local<Number> pWidth = Local<Number>::Cast( obj->Get( String::NewSymbol("width") ) );
+  if ( pWidth->NumberValue() > 1 || pWidth->NumberValue() < 0) {
+    return THROW_ERROR_EXCEPTION("\"width\" should be Number with the value between 0 and 1");
+  }
+
+  Local<Number> pHeight = Local<Number>::Cast( obj->Get( String::NewSymbol("height") ) );
+  if ( pHeight->NumberValue() > 1 || pHeight->NumberValue() < 0) {
+    return THROW_ERROR_EXCEPTION("\"height\" should be Number with the value between 0 and 1");
+  }
+
+  Local<Number> pTop = Local<Number>::Cast( obj->Get( String::NewSymbol("top") ) );
+  if ( pTop->NumberValue() > 1 || pTop->NumberValue() < 0) {
+    return THROW_ERROR_EXCEPTION("\"top\" should be Number with the value between 0 and 1");
+  }
+
+  Local<Number> pLeft = Local<Number>::Cast( obj->Get( String::NewSymbol("left") ) );
+  if ( pLeft->NumberValue() > 1 || pLeft->NumberValue() < 0) {
+    return THROW_ERROR_EXCEPTION("\"left\" should be Number with the value between 0 and 1");
+  }
+
+  int debug = obj->Get( String::NewSymbol("debug") )->Uint32Value();
+  if (debug) printf( "debug: on\n" );
+
+  Magick::Blob srcBlob( node::Buffer::Data(srcData), node::Buffer::Length(srcData) );
+
+  Magick::Image image;
+  try {
+    image.read( srcBlob );
+  }
+  catch (std::exception& err) {
+    std::string message = "image.read failed with error: ";
+    message            += err.what();
+    return THROW_ERROR_EXCEPTION(message.c_str());
+  }
+  catch (...) {
+    return THROW_ERROR_EXCEPTION("unhandled error");
+  }
+
+  if (debug) printf("original width,height: %d, %d\n", (int) image.columns(), (int) image.rows());
+
+  unsigned int width = pWidth->NumberValue()*image.columns();
+  if (debug) printf( "width: %d\n", width );
+
+  unsigned int height = pHeight->NumberValue()*image.rows();
+  if (debug) printf( "height: %d\n", height );
+
+  unsigned int top = pTop->NumberValue()*image.columns();
+  if (debug) printf( "top: %d\n", top );
+
+  unsigned int left = pLeft->NumberValue()*image.rows();
+  if (debug) printf( "left: %d\n", left );
+
+  Local<Value> formatValue = obj->Get( String::NewSymbol("format") );
+  String::AsciiValue format( formatValue->ToString() );
+  if ( ! formatValue->IsUndefined() ) {
+    if (debug) printf( "format: %s\n", *format );
+    image.magick( *format );
+  }
+
+  if ( width || height ) {
+    if ( ! width  ) { width  = image.columns(); }
+    if ( ! height ) { height = image.rows();    }
+
+    // do resize
+    // ^ : Fill Area Flag ('^' flag)
+    // is not implemented in Magick++
+    // and gravity: center, extent doesnt look like working as exptected
+    // so we do it ourselves
+
+    // keep aspect ratio, get the exact provided size, crop top/bottom or left/right if necessary
+    double aspectratioExpected = (double)height / (double)width;
+    double aspectratioOriginal = (double)image.rows() / (double)image.columns();
+    unsigned int xoffset = 0;
+    unsigned int yoffset = 0;
+    unsigned int resizewidth;
+    unsigned int resizeheight;
+    if ( aspectratioExpected > aspectratioOriginal ) {
+      // expected is taller
+      resizewidth  = (unsigned int)( (double)height / (double)image.rows() * (double)image.columns() + 1. );
+      resizeheight = height;
+      xoffset      = (unsigned int)( (resizewidth - width) / 2. );
+      yoffset      = 0;
+    }
+    else {
+      // expected is wider
+      resizewidth  = width;
+      resizeheight = (unsigned int)( (double)width / (double)image.columns() * (double)image.rows() + 1. );
+      xoffset      = 0;
+      yoffset      = (unsigned int)( (resizeheight - height) / 2. );
     }
 
-    int debug = obj->Get( String::NewSymbol("debug") )->Uint32Value();
-    if (debug) printf( "debug: on\n" );
+    if (debug) printf( "resize to: %d, %d\n", resizewidth, resizeheight );
+    Magick::Geometry resizeGeometry( resizewidth, resizeheight, 0, 0, 0, 0 );
+    image.resize( resizeGeometry );
 
-    Magick::Blob srcBlob( node::Buffer::Data(srcData), node::Buffer::Length(srcData) );
+    // limit canvas size to cropGeometry
+    if (debug) printf( "crop to: %d, %d, %d, %d\n", width, height, xoffset, yoffset );
+    Magick::Geometry cropGeometry( width, height, xoffset, yoffset, 0, 0 );
 
-    Magick::Image image;
-    try {
-        image.read( srcBlob );
+    Magick::Color transparent( "white" );
+    if ( strcmp( *format, "PNG" ) == 0 ) {
+      // make background transparent for PNG
+      // JPEG background becomes black if set transparent here
+      transparent.alpha( 1. );
     }
-    catch (std::exception& err) {
-        std::string message = "image.read failed with error: ";
-        message            += err.what();
-        return THROW_ERROR_EXCEPTION(message.c_str());
-    }
-    catch (...) {
-        return THROW_ERROR_EXCEPTION("unhandled error");
-    }
+    image.extent( cropGeometry, transparent );
+    if (debug) printf( "resized to: %d, %d\n", (int)image.columns(), (int)image.rows() );
+  }
 
-    if (debug) printf("original width,height: %d, %d\n", (int) image.columns(), (int) image.rows());
+  unsigned int quality = obj->Get( String::NewSymbol("quality") )->Uint32Value();
+  if ( quality ) {
+    if (debug) printf( "quality: %d\n", quality );
+    image.quality( quality );
+  }
 
-    unsigned int width = obj->Get( String::NewSymbol("width") )->Uint32Value();
-    if (debug) printf( "width: %d\n", width );
+  Magick::Blob dstBlob;
+  image.write( &dstBlob );
 
-    unsigned int height = obj->Get( String::NewSymbol("height") )->Uint32Value();
-    if (debug) printf( "height: %d\n", height );
-
-    Local<Value> resizeStyleValue = obj->Get( String::NewSymbol("resizeStyle") );
-    const char* resizeStyle = "aspectfill";
-    String::AsciiValue resizeStyleAsciiValue( resizeStyleValue->ToString() );
-    if ( ! resizeStyleValue->IsUndefined() ) {
-        resizeStyle = *resizeStyleAsciiValue;
-    }
-    if (debug) printf( "resizeStyle: %s\n", resizeStyle );
-
-    Local<Value> formatValue = obj->Get( String::NewSymbol("format") );
-    String::AsciiValue format( formatValue->ToString() );
-    if ( ! formatValue->IsUndefined() ) {
-        if (debug) printf( "format: %s\n", *format );
-        image.magick( *format );
-    }
-
-    if ( width || height ) {
-        if ( ! width  ) { width  = image.columns(); }
-        if ( ! height ) { height = image.rows();    }
-
-        // do resize
-        if ( strcmp( resizeStyle, "aspectfill" ) == 0 ) {
-            // ^ : Fill Area Flag ('^' flag)
-            // is not implemented in Magick++
-            // and gravity: center, extent doesnt look like working as exptected
-            // so we do it ourselves
-
-            // keep aspect ratio, get the exact provided size, crop top/bottom or left/right if necessary
-            double aspectratioExpected = (double)height / (double)width;
-            double aspectratioOriginal = (double)image.rows() / (double)image.columns();
-            unsigned int xoffset = 0;
-            unsigned int yoffset = 0;
-            unsigned int resizewidth;
-            unsigned int resizeheight;
-            if ( aspectratioExpected > aspectratioOriginal ) {
-                // expected is taller
-                resizewidth  = (unsigned int)( (double)height / (double)image.rows() * (double)image.columns() + 1. );
-                resizeheight = height;
-                xoffset      = (unsigned int)( (resizewidth - width) / 2. );
-                yoffset      = 0;
-            }
-            else {
-                // expected is wider
-                resizewidth  = width;
-                resizeheight = (unsigned int)( (double)width / (double)image.columns() * (double)image.rows() + 1. );
-                xoffset      = 0;
-                yoffset      = (unsigned int)( (resizeheight - height) / 2. );
-            }
-
-            if (debug) printf( "resize to: %d, %d\n", resizewidth, resizeheight );
-            Magick::Geometry resizeGeometry( resizewidth, resizeheight, 0, 0, 0, 0 );
-            image.resize( resizeGeometry );
-
-            // limit canvas size to cropGeometry
-            if (debug) printf( "crop to: %d, %d, %d, %d\n", width, height, xoffset, yoffset );
-            Magick::Geometry cropGeometry( width, height, xoffset, yoffset, 0, 0 );
-
-            Magick::Color transparent( "white" );
-            if ( strcmp( *format, "PNG" ) == 0 ) {
-                // make background transparent for PNG
-                // JPEG background becomes black if set transparent here
-                transparent.alpha( 1. );
-            }
-            image.extent( cropGeometry, transparent );
-        }
-        else if ( strcmp ( resizeStyle, "aspectfit" ) == 0 ) {
-            // keep aspect ratio, get the maximum image which fits inside specified size
-            char geometryString[ 32 ];
-            sprintf( geometryString, "%dx%d", width, height );
-            if (debug) printf( "resize to: %s\n", geometryString );
-            image.resize( geometryString );
-        }
-        else if ( strcmp ( resizeStyle, "fill" ) == 0 ) {
-            // change aspect ratio and fill specified size
-            char geometryString[ 32 ];
-            sprintf( geometryString, "%dx%d!", width, height );
-            if (debug) printf( "resize to: %s\n", geometryString );
-            image.resize( geometryString );
-        }
-        else {
-            return THROW_ERROR_EXCEPTION("resizeStyle not supported");
-        }
-        if (debug) printf( "resized to: %d, %d\n", (int)image.columns(), (int)image.rows() );
-    }
-
-    unsigned int quality = obj->Get( String::NewSymbol("quality") )->Uint32Value();
-    if ( quality ) {
-        if (debug) printf( "quality: %d\n", quality );
-        image.quality( quality );
-    }
-
-    Magick::Blob dstBlob;
-    image.write( &dstBlob );
-
-    node::Buffer* retBuffer = node::Buffer::New( dstBlob.length() );
-    memcpy( node::Buffer::Data( retBuffer->handle_ ), dstBlob.data(), dstBlob.length() );
-    return scope.Close( retBuffer->handle_ );
+  node::Buffer* retBuffer = node::Buffer::New( dstBlob.length() );
+  memcpy( node::Buffer::Data( retBuffer->handle_ ), dstBlob.data(), dstBlob.length() );
+  return scope.Close( retBuffer->handle_ );
 }
 
 void init(Handle<Object> target) {
-    NODE_SET_METHOD(target, "convert", Convert);
+  NODE_SET_METHOD(target, "convert", Convert);
+  NODE_SET_METHOD(target, "crop", Crop);
 }
 
 NODE_MODULE(imagemagick, init);
